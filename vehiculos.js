43d58ce323b42536356b210353b52f38d6b20ed4
LIHC1305 <lherrera7@ucol.mx>
2026-02-13 08:39:32 -0600

    V3

---
+++ vehiculos.js
@@ -0,0 +1,19 @@
+let n = parseInt(prompt("Ingresa un número:"));
+
+switch(n) {
+    case 1:
+        alert("Coche");
+        break;
+    case 2:
+        alert("Moto");
+        break;
+    case 3:
+        alert("Bicicleta");
+        break;
+    case 4:
+        alert("Camión");
+        break;
+    default:
+        alert("No recibí una opción válida");
+        break;
+}
